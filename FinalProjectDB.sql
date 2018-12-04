68276c4ad11c704f1262cd4618254725a1e5fdc4
Jordan Brainard <35605866+jordanbrainard@users.noreply.github.com>
2018-12-04 03:33:53 -0600

    Basic code for SQL database

---
+++ FinalProjectDB.sql
@@ -0,0 +1,4 @@
+CREATE TABLE PINS(pinID int, latitude double, longitude double, pinName varchar(20), pinDesc varchar(35), primary key (pinID));
+
+INSERT into PINS VALUES(1, 48.863, -96.98, "TEST PIN 1", "TEST DESC 1");
+INSERT into PINS VALUES(2, 48.823, -96.65, "TEST PIN 2", "TEST DESC 2");
